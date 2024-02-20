5877ef1aaa247553708c842712d84fc07cf298d2
Vladimir Danilin <volodyadanilin@gmail.com>
2024-02-19 23:19:04 -0500

    IProductService Interface Added

---
+++ Data/Services/IProductService.cs
@@ -0,0 +1,17 @@
+﻿using eCommerceWebApp.Models;
+
+namespace eCommerceWebApp.Data.Services
+{
+    public interface IProductService
+    {
+        IEnumerable<Product> GetAllProducts();
+
+        Product GetById(int id);
+
+        void Add(Product product);
+
+        Product Update(int id, Product newProduct);
+
+        void Delete(int id);
+    }
+}
