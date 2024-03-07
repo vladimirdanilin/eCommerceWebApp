e42ec397389f960c54f75e3e6668cdd6d74d8d91
Vladimir Danilin <volodyadanilin@gmail.com>
2024-03-06 23:48:03 -0500

    IAddressService Added

---
+++ Data/Services/IAddressService.cs
@@ -0,0 +1,18 @@
+﻿using eCommerceWebApp.Models;
+
+namespace eCommerceWebApp.Data.Services
+{
+    public interface IAddressService
+    {
+        Task<Address> GetAddressByIdAsync(int id);
+
+        Task<List<Address>> GetUserAddressesByUserIdAsync(int userId);
+
+        Task<Address> AddAddressAsync(Address address);
+
+        Task<Address> EditAddressAsync(Address address);
+
+        Task<Address> DeleteAddressAsync(int addressId);
+
+    }
+}
